b06af36c6e263c88f4fa024b5930f5a44f5fd0f8
Gilad Leifman <leifmangilad@gmail.com>
2023-11-13 09:23:59 +0200

    more

--- setup.sh
+++ setup.sh
@@ -7,15 +7,14 @@ sudo apt-get install -y vim zsh git openssh-server npm curl daemonize dbus-user-
 # Install specific versions or packages from URLs
 sudo apt-get install -y fc-cache fonts-powerline fonts-hack-ttf
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+curl -LO https://dl.google.com/go/go1.20.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
 sudo dpkg -i ripgrep_13.0.0_amd64.deb
 
-# Configure PATH
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-echo 'export PATH=$PATH:/home/giladl/go/bin' >> ~/.profile
-echo 'export PATH=$PATH:/home/giladl/.local/bin' >> ~/.profile
-source ~/.profile
+# Download and install Neovim
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
+sudo apt install ./nvim-linux64.deb
 
 # Clone and install Powerline Fonts
 git clone https://github.com/powerline/fonts.git
@@ -30,15 +29,18 @@ sudo service ssh restart
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# Neovim installation
-mv /mnt/c/Users/Gilad/Downloads/nvim-linux64.deb /tmp
-sudo apt install /tmp/nvim-linux64.deb
-
 # Systemd namespace scripts
+# Note: You'll need to provide the contents of 'start-systemd-namespace' and 'enter-systemd-namespace' scripts
 echo 'source /usr/sbin/start-systemd-namespace' | sudo tee -a /etc/bash.bashrc
 sudo cp /path/to/start-systemd-namespace /usr/sbin/
 sudo cp /path/to/enter-systemd-namespace /usr/sbin/
 sudo chmod +x /usr/sbin/enter-systemd-namespace
+
+# Configure PATH
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+echo 'export PATH=$PATH:/home/giladl/go/bin' >> ~/.profile
+echo 'export PATH=$PATH:/home/giladl/.local/bin' >> ~/.profile
+source ~/.profile
 
 # Additional configurations
 echo 'additional configurations here' >> ~/.bashrc
