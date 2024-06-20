61ffdb0a3488c17098aae001ca009901866a9ad6
Gilad Leifman <91918361+giladl-neosec@users.noreply.github.com>
2024-06-20 17:30:25 +0300

    mac file

---
+++ mac.sh
@@ -0,0 +1,5 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install --cask rectangle
+brew install --cask linearmouse
+brew install --cask sublime-text
+brew install fzf kubectl kubens kubectx terraform gh go python@3.10 python@3.11 python@3.12 htop awscli session-manager-plugin cloc yq docker-machine docker sha256sum aws-iam-authenticator git-lfs obsidian --cask warp duckdb graphviz httrack
