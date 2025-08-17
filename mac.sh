906e841f24d7bab52af4171fd43d401434e18c25
Gilad Leifman <leifmangilad@gmail.com>
2025-08-17 11:50:15 +0300

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,4 +1,9 @@
+softwareupdate --install-rosetta --agree-to-license
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install --cask fzf kubectl kubens kubectx terraform gh go python@3.10 python@3.11 python@3.12 htop awscli session-manager-plugin cloc yq docker-machine docker sha256sum aws-iam-authenticator git-lfs rectangle google-chrome jetbrains-toolbox whatsapp spotify sublime-text obsidian warp duckdb graphviz httrack stremio git gh iterm2 neovim raycast
+brew install fzf kubectl kubectx terraform gh go python@3.11 python@3.12 python@3.13 htop awscli session-manager-plugin cloc yq docker-machine docker coreutils aws-iam-authenticator git-lfs duckdb graphviz httrack git gh iterm2 neovim raycast
+brew services start docker-machine
+git lfs install
+brew install --cask google-chrome || true
+brew install --cask rectangle jetbrains-toolbox whatsapp spotify sublime-text obsidian warp stremio 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-mkdir ~/projects
+mkdir ~/projects || true
