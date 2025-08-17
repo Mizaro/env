d9d0723ced6af57480f58a94aa4b12c512d042eb
Gilad Leifman <leifmangilad@gmail.com>
2025-08-17 10:19:20 +0300

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,5 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install --cask rectangle
-brew install --cask linearmouse
-brew install --cask sublime-text
-brew install fzf kubectl kubens kubectx terraform gh go python@3.10 python@3.11 python@3.12 htop awscli session-manager-plugin cloc yq docker-machine docker sha256sum aws-iam-authenticator git-lfs obsidian --cask warp duckdb graphviz httrack
+brew install --cask fzf kubectl kubens kubectx terraform gh go python@3.10 python@3.11 python@3.12 htop awscli session-manager-plugin cloc yq docker-machine docker sha256sum aws-iam-authenticator git-lfs rectangle google-chrome jetbrains-toolbox whatsapp spotify sublime-text obsidian warp duckdb graphviz httrack stremio git gh iterm2 neovim raycast
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+mkdir projects
