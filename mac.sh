14c93577b0cc226dc865e40b3a64ab051bb59c6a
Gilad Leifman <leifmangilad@gmail.com>
2025-08-17 12:46:02 +0300

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,7 +1,7 @@
 softwareupdate --install-rosetta --agree-to-license
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerSwipeGesture -int 1
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install fzf kubectl kubectx terraform gh go python@3.11 python@3.12 python@3.13 htop awscli session-manager-plugin cloc yq docker-machine docker coreutils aws-iam-authenticator git-lfs duckdb graphviz httrack git gh iterm2 neovim raycast
-
 brew services start docker-machine
 git lfs install
 brew install --cask google-chrome || true
